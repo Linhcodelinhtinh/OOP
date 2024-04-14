f02e78c60f6848ac54e1d70ffa4fc84df67cd5b6
Linhcodelinhtinh <23020392@vnu.edu.vn>
2024-04-14 23:06:47 +0700

    Update No2FlipX.java

--- HW2/No2FlipX.java
+++ HW2/No2FlipX.java
@@ -1,4 +1,22 @@
 package OOP.HW2;
 
+import java.awt.Color;
+
 public class No2FlipX {
+
+    public static void main(String[] args) {
+        Picture pic = new Picture(args[0]);
+        int width  = pic.width();
+        int height = pic.height();
+        pic.show();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width / 2; x++) {
+                Color c1 = pic.get(x, y);
+                Color c2 = pic.get(width - x - 1, y);
+                pic.set(x, y, c2);
+                pic.set(width - x - 1, y, c1);
+            }
+        }
+        pic.show();
+    }
 }
