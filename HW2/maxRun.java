39c391577fb90e4ffa7fdf4d2251a66df94804c1
Linhcodelinhtinh <23020392@vnu.edu.vn>
2024-04-22 09:28:21 +0700

    little bit more on hw2

---
+++ HW2/maxRun.java
@@ -0,0 +1,28 @@
+package OOP.HW2;
+
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+
+public class maxRun {
+    public static int max_Run(String str){
+        int count = 1, maxCount = 1;
+        str = str + "?";
+        char[] chars = str.toCharArray();
+        for(int i=0;i<str.length() - 1;i++){
+            if(chars[i]==chars[i+1]){
+                count++;
+            }
+            else {
+                maxCount = max(maxCount, count);
+                count = 1;
+            }
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(max_Run(str));
+    }
+}
